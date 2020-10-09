3da916e9b29dc0920bd1a2f4820fe5398d8719c4
patches9119 <pbsanders1@gmail.com>
2020-10-09 14:44:40 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.BOT_TOKEN);
+
+// NzY0MjMyMTg2ODUyNDA5Mzc0.X4DQsA.kFKdXM2-rH71AY7X9fG2Vvfi00s
+
+const answers = [
+    ' is reaching critical sus. Immediete vote recomended.',
+    ' is extremely sus.',
+    ' probably vented.',
+    ' took a little too long on trash.',
+    ' is sus',
+    ' has not been in range of cams for me to see.',
+    ' isn\'t very sus.',
+    ' did medscan.'
+]
+
+client.on('message', message => {
+	console.log(message.content);
+    if (message.content.includes('!sus')) {
+        // send back "Pong." to the channel the message was sent in
+        var response = message.content.slice(4);
+        var tag = answers[Math.floor(Math.random() * 9)];
+        message.channel.send(response + tag);
+    }
+});
