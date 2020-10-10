edcc5511d4f8559b52acd5bdc9ef56f8db34ff8f
patches9119 <pbsanders1@gmail.com>
2020-10-10 08:24:09 -0700

    reworked response building to avoid sending blank responses

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ client.once('ready', () => {
 client.login(process.env.BOT_TOKEN);
 
 
+const answers = [
+    ' literally killed green in front of me.',
+    ' probably faked upload.',
+    ' probably vented.',
+    ' took a little too long on trash.',
+    ' is sus.',
+    ' hasn\'t been on cams all round.',
+    ' did medscan.',
+]
 
 
 
 client.on('message', message => {
     if (message.content.includes('!sus')) {
         var response = message.content.slice(4);
-        var answers = [
-            response + ' literally killed green in front of me.',
-            response + ' probably faked upload.',
-            response + ' probably vented.',
-            response + ' took a little too long on trash.',
-            response + ' is sus.',
-            response + ' hasn\'t been on cams all round.',
-            response + ' did medscan.',
-        ]
         var tag = answers[Math.floor(Math.random() * 8)];
-        setTimeout(() => { message.channel.send(tag); }, 200);
+        message.channel.send(response + tag);
         
     }
 });
